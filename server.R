ec5086720a8aba53df43855a734846aaedb6142e
Gabriel Muñoz <fgabriel1891@gmail.com>
2017-04-13 19:18:56 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -1,92 +1,173 @@
+# 
+# # This is the server logic for a Shiny web application.
+# # You can find out more about building applications with Shiny here:
+# #
+# # http://shiny.rstudio.com
+# #
+# 
+# library(shiny)
+# library(monkeylearn)
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+shinyServer(function(input, output, session) {
 
-library(shiny)
 
-shinyServer(function(input, output) {
-  
-  
 read <- eventReactive(input$dataset,{
-  
+
 # Read the pdf
  fulltext::ft_extract(input$dataset)
- 
-  
+
+
 })
 
 # Custom fucntion to index
-
-Index <- function(read,verbatim, dictionary) {
-  
-  IndexText <- list()
-  for(i in 1:length(verbatim$offsetend)){
-    IndexText[[i]] <- stringr::str_sub(iconv(enc2utf8(read),sub="byte"),verbatim$offsetstart[i]-50,verbatim$offsetend[i]+250)
-    
-  }
-  matches <- c()
-  ToMatch <- c()
-  for ( i in 1:length(IndexText)){
-    matches[[i]] <- grepl(paste(dictionary$term, collapse = "|"),IndexText[i])
-  }
-  df <- c()
-  df$text <- IndexText[matches==TRUE]
-  df$which <- which(matches == TRUE)
-  return(df)
-}
-
+#
 
 #return( print(IndexText[which(matches[[i]] == TRUE)]))
 
+Index <- function(read,verbatim, dictionary) {
+
+  IndexText <- list()
+  for(i in 1:length(verbatim$offsetend)){
+    IndexText[i] <- stringr::str_sub(iconv(enc2utf8(read()$data),sub="byte"),
+                                     verbatim$offsetstart[i]-150,
+                                     verbatim$offsetend[i]+300)
+
+  }
+  matches <- c()
+  ToMatch <- c()
+
+  for ( i in 1:length(IndexText)){
+    matches[[i]] <- grepl(paste(dictionary[,1], collapse = "|"),IndexText[i])
+  }
+  df <- c()
+  df$text <- unlist(IndexText[matches==TRUE])
+  df$where <- verbatim$offsetstart[which(matches == TRUE)]
 
 
-  
+  return(df)
+}
+
+# Custom function to retrieve dictionary matches
+
+termcount <- function(dictionary, text){
+
+  count <-c()
+  for (i in 1:length(unlist(dictionary))){
+    count$term[i] <- dictionary[i,]
+    count$count[i] <- length(grep(dictionary[i,], iconv(enc2utf8(text),sub="byte")))
+  }
+
+  b <- data.frame(count)
+  return(b[order(b$count, decreasing = T),]
+  )
+}
+
+
 
 # Tab with Scientific Names found and count
 
   output$data_table <- renderTable({
     #     Find scientific names
     scinames.d <- taxize::scrapenames(text = read()$data, all_data_sources = TRUE)
-    
-    sciname <- scinames.d$data$scientificname # output 
+
+    sciname <- scinames.d$data$scientificname # output
     table(sciname,dnn = "Count")[order(table(sciname), decreasing = T)]
-    
+
   })
-  
+
 # Tab to show the dictionary
-    output$dictionary <- renderTable({
+    output$dictionary <- renderPlot({
       # Read the dictionary
-      dictio <- read.csv(input$dictionary, header = TRUE)
-      
+      dictio <- read.csv(input$dictionary, header = TRUE, stringsAsFactors = F)
+      c <- termcount(dictio, read()$data)
+      wordcloud::wordcloud(freq = c$count, words = c$term, 
+                           max.words = 10, main = "Common terms from dictionary" )
 
     })
-    
-# Tab to show the indexed results
 
-     
+    # Tab to show the indexed results
+
+
     output$Indexed.version <- renderTable({
-      dictio <- read.csv(input$dictionary, header = TRUE)
+      dictio <- read.csv(input$dictionary, header = TRUE, stringsAsFactors = F)
       scinames.d <- taxize::scrapenames(text = read()$data, all_data_sources = TRUE)
-      verbatim <-scinames.d$data[c("verbatim","offsetend", "offsetstart")]
-    table(unlist( Index(read()$data, verbatim,dictio)$text),deparse.level = 0)
-    
+      verbatim <- scinames.d$data[c("verbatim","offsetend", "offsetstart")]
+    table("Species" = unlist( Index(read()$data, verbatim,dictio)$text))
+
     })
-    
+
     output$plot <- renderPlot({
       dictio <- read.csv(input$dictionary, header = TRUE)
       scinames.d <- taxize::scrapenames(text = read()$data, all_data_sources = TRUE)
       verbatim <-scinames.d$data[c("verbatim","offsetend", "offsetstart")]
-      matches <- Index(read()$data, verbatim,dictio)$which
-      
-      
-      plot(verbatim$offsetstart[matches], 
-           xlab = "String match rank", ylab = "Position in the text article (in characters)", 
+      matches <- Index(read()$data, verbatim,dictio)$where
+
+
+      plot(matches,
+           xlab = "String match rank", ylab = "Article lenght",
            ylim = c(0,nchar(iconv(enc2utf8(read()$data),sub="byte"))),
            col = "#5ba966",pch = 16,
            main = "position on the text")
-      legend("bottomright", "Position of record \n along the text",pch = 16, col = "#5ba966", bty = "y" )
+      legend("bottomright", "Position of record \n along the text",pch = 16, 
+             col = "#5ba966", bty = "y" )
     })
+
 })
+
+# 
+# #coord
+# # 
+# # out1 <- monkeylearn::monkeylearn_extract(texts$data, extractor_id = "ex_isnnZRbS", key=key)
+# # out2 <- out1$entity[out1$tag == "LOCATION"]
+# # 
+# # coord <- dismo::geocode(out2)
+# # leaflet(coord) %>%
+# #   addMarkers(lng = coord$longitude, lat = coord$latitude)
+# 
+# 
+# # texts <- fulltext::ft_extract(path1[1])
+# # doct <- read.csv("dic/dictionary.csv", header = T, stringsAsFactors = F)
+# # verb <-  taxize::scrapenames(text = texts$data, all_data_sources = TRUE)
+# # verb <- verb$data
+# # Index2(iconv(enc2utf8(texts$data)), verbatim = verb$data, dictionary = doct)
+# # 
+# # 
+# # 
+# # 
+# # 
+# # Index2 <- function(read,verbatim, dictionary) {
+# # 
+# #   IndexText <- list()
+# #   for(i in 1:length(verbatim$offsetend)){
+# #     IndexText[i] <- stringr::str_sub(iconv(enc2utf8(read),sub="byte"),
+# #                                      verbatim$offsetstart[i]-150,
+# #                                      verbatim$offsetend[i]+300)
+# # 
+# #   }
+# #   matches <- c()
+# #   ToMatch <- c()
+# # 
+# #   for ( i in 1:length(IndexText)){
+# #     matches[[i]] <- grepl(paste(dictionary[,1], collapse = "|"),IndexText[i])
+# #   }
+# #   df <- c()
+# #   df$text <- unlist(IndexText[matches==TRUE])
+# #   df$where <- verbatim$offsetstart[which(matches == TRUE)]
+# # 
+# # 
+# #   return(df)
+# # }
+# # 
+# # 
+# # 
+# # ww <- stringr::str_sub(iconv(enc2utf8(texts$data),sub="byte"),
+# #                        verb$offsetstart-150,
+# #                        verb$offsetend+300)
+# # 
+# # wwe <- c("", "ripen", "noripen", "fika ret", "wood rail, , National, , Platalea ajaja, , Roseate spoonbill, , International, , Burhinus bistriatus, , Double-striped thick-knee, , International, , Thalasseus elegans, , Elegant tern, , International, , Ardea alba, , Great egret, , International, , Ardea herodias, , Great blue heron, , International, , Mesembrinbis cayennensis, , Green ibis, , International, , 4 BioScience <b7> XXXX XXXX / Vol. XX No. X, , http://bioscience.oxfordjournals.org, , Overview Artic")
+# # match <- grepl(paste(doct[,1], collapse = "|"),wwe)
+# 
+# 
+# 
+# 
+# 
--- ui.R
+++ ui.R
@@ -9,38 +9,83 @@
 #
 
 library(shiny)
-
-path <- "/Users/Gabriel/Desktop/ShinyAPPS/LiteratureReader/test"
+library(leaflet)
+library(shinythemes)
+path <- "/Users/Gabriel/Desktop/ShinyAPPS/LiteratureReader/articles/"
 filenames<-list.files(path)
-path1 <- paste("/Users/Gabriel/Desktop/ShinyAPPS/LiteratureReader/test","/", filenames, sep = "")
+path1 <- paste("/Users/Gabriel/Desktop/ShinyAPPS/LiteratureReader/articles/","/", filenames, sep = "")
+names(path1) <- filenames
+
 
 dictionary.path <- "/Users/Gabriel/Desktop/ShinyAPPS/LiteratureReader/dic"
 filenames.dic<-list.files(dictionary.path)
 path2 <- paste("/Users/Gabriel/Desktop/ShinyAPPS/LiteratureReader/dic","/", filenames.dic, sep = "")
+names(path2) <- filenames.dic
 
-shinyUI(pageWithSidebar(
-  headerPanel(
-    "Literature reader"
-  ),
-  sidebarPanel(
-    selectInput(inputId = "dataset",
-                label = "Select article to read",
-                path1
-    ),
-    selectInput(inputId = "dictionary",
-                label = "Provide a thesaurus",
-                path2
-    )   
-  ),
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Species names",tableOutput("data_table"),id="myTab"),
-      tabPanel("Dictionary",tableOutput("dictionary"),id="myTab1"),
-      tabPanel("Indexed version",tableOutput("Indexed.version"),id="Index"),
-      tabPanel("plot",plotOutput("plot"),id="myplot"),
-      id="Plot_Data"
-    )
+key <- "cf0b9da7695ba68256cd61ee7fe04cbf84ae4ede" # To change this for "accept other keys"
+
+shinyUI(navbarPage("Ecological Event Miner", id ="nav",
+                  
+                   tabPanel("Instructions",
+                            fluidPage( theme = shinytheme("yeti"), 
+                              sidebarLayout(
+                                sidebarPanel(
+                                selectInput(inputId = "dataset",
+                                                       label = "Select article to read",
+                                                       path1),
+                                selectInput(inputId = "dictionary",
+                                          label = "Provide a thesaurus",
+                                          path2),
+                                img(src='logo.png', align = "right"),
+                                p("This shiny app is currently in development and under the CreativeCommons CC BY-NC-SA License."),
+                                p("Contact Person: Gabriel Muñoz | fgabriel1891@gmail.com")
+                            ),
+                            mainPanel(
+                              includeMarkdown("www/01-Instructions.Rmd")
+                            )
+                          )
+                        )
+                      ),
+                   tabPanel("Species names",
+                            splitLayout(
+                              plotOutput("dictionary"),
+                              tableOutput("data_table")
+                              )
+                            ),
+                   tabPanel("Indexed version",
+                            fluidPage(
+                              sidebarLayout(
+                                sidebarPanel(
+                                  plotOutput("plot"),id="myplot"),
+                              mainPanel(
+                                tableOutput("Indexed.version"),id="Index")
+                              )
+                            )
+                   )
   )
-))
+)
+                   
+#                    
+#     
+#   sidebarPanel(
+#     
+#     selectInput(inputId = "key",
+#                 label = "Provide a key for MonkeyLearn API" ,
+#                 key),
+#     
+#     img(src='logo.png', align = "right"),
+#     br(),
 
-    
+#   mainPanel(
+#     h2("Ecological Events Miner"),
+#     
+#     tabsetPanel(
+#       
+#       tabPanel("Dictionary",,id="myTab1"),
+#       
+#     )
+#   )
+#  )
+# )
+# 
+#     
